ec885a811d3c4cf757b84b20a7a9619ecc5fa260
최규원 <rbdnjs1011@naver.com>
2024-04-15 17:02:11 +0900

    0415/2

--- summary.py
+++ summary.py
@@ -1,7 +1,18 @@
 from openai import OpenAI
 import os
+import re
 
 client = OpenAI(api_key="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")
+
+
+def extract_link(content):
+    link_pattern = r'Link: (.*)$'
+    match = re.search(link_pattern, content, re.MULTILINE)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
 
 def summarize_files(folder_path):
     file_paths = [os.path.join(folder_path, file) for file in os.listdir(
@@ -16,17 +27,19 @@ def summarize_files(folder_path):
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "너는 뉴스를 핵심 문장으로 요약해주는 문장의 끝맺음이 확실한 훌륭한 기자이다."},
-                {"role": "user", "content": f"{content}를 요약해줘."}
+                {"role": "user", "content": f"{content}를 3~4줄 이하로 요약해줘."}
             ],
             max_tokens=300,
             stream=True,
         )
 
         summary = ""
+        link = extract_link(content)  # 링크 추출
         for chunk in response:
             if chunk.choices[0].delta.content is not None:
                 summary += chunk.choices[0].delta.content
 
-        summaries.append({'file_path': file_path, 'summary': summary})
+        summaries.append(
+            {'file_path': file_path, 'summary': summary, 'link': link})
 
     return summaries
