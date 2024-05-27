a20c6d5685204185c48c2150cec66dd58a49c829
최규원 <rbdnjs1011@naver.com>
2024-05-27 12:48:11 +0900

    헤드라인 추가

--- summary.py
+++ summary.py
@@ -4,6 +4,13 @@ import re
 
 client = OpenAI(api_key="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")
 
+def extract_title(content):
+    title_pattern = r'Title: (.*)$'
+    match = re.search(title_pattern, content, re.MULTILINE)
+    if match:
+        return match.group(1)
+    else:
+        return None
 
 def extract_link(content):
     link_pattern = r'Link: (.*)$'
@@ -13,10 +20,8 @@ def extract_link(content):
     else:
         return None
 
-
 def summarize_files(folder_path):
-    file_paths = [os.path.join(folder_path, file) for file in os.listdir(
-        folder_path) if file.endswith('.txt')]
+    file_paths = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.txt')]
     summaries = []
 
     for file_path in file_paths:
@@ -34,12 +39,12 @@ def summarize_files(folder_path):
         )
 
         summary = ""
-        link = extract_link(content)  # 링크 추출
+        title = extract_title(content)  # 타이틀 추출
+        link = extract_link(content)    # 링크 추출
         for chunk in response:
             if chunk.choices[0].delta.content is not None:
                 summary += chunk.choices[0].delta.content
 
-        summaries.append(
-            {'file_path': file_path, 'summary': summary, 'link': link})
+        summaries.append({'file_path': file_path, 'summary': summary, 'link': link, 'title': title})
 
     return summaries
